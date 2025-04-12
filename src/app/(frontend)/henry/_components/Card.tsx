d4f4c78998cf589c51376faf8124f5131496f8e6
Henry Gao <henrygao48@gmail.com>
2025-04-12 17:32:43 +1200

    Add bg image and simple motion animations

--- src/app/(frontend)/henry/_components/Card.tsx
+++ src/app/(frontend)/henry/_components/Card.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { AnimatePresence, motion } from 'motion/react'
+import { IoIosClose } from 'react-icons/io'
 
 const links = [
   {
@@ -18,36 +21,76 @@ const links = [
 ]
 
 export default function Card() {
+  const [isCardVisible, setIsCardVisible] = useState(true)
+
   return (
-    <div className="flex flex-col items-center bg-neutral-800 p-8 text-white rounded-lg">
-      <Image
-        src="https://static-cdn.jtvnw.net/jtv_user_pictures/c5a9df33-4e9e-4df6-8dba-c3746041b95e-profile_image-300x300.png"
-        width={80}
-        height={80}
-        alt="Henry's Profile Image"
-        className="rounded-full"
-      />
-
-      <div className="flex flex-col items-center my-4">
-        <h1 className="text-2xl font-semibold ">Henry Gao</h1>
-        <p className="text-lime-400 text-xs">Auckland, New Zealand</p>
-      </div>
-
-      <p className="text-xs text-neutral-400 mb-4">"Full-stack developer and retired gamer"</p>
-
-      <div className="flex flex-col gap-2 w-full">
-        {links.map((link) => (
-          <a
-            target="_blank"
-            href={link.link}
-            rel="noopener noreferrer"
-            key={link.text}
-            className="bg-neutral-700 w-full text-center rounded-md py-1 text-sm font-semibold tracking-wide"
+    <>
+      <AnimatePresence initial={false}>
+        {isCardVisible ? (
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            key="card"
+            className="relative flex flex-col items-center bg-neutral-800 p-8 text-white rounded-lg scale-125"
           >
-            {link.text}
-          </a>
-        ))}
-      </div>
-    </div>
+            <motion.div
+              whileHover={{ scale: 1.3 }}
+              whileTap={{ scale: 0.8 }}
+              className="absolute top-2 right-2 hover:cursor-pointer"
+              onClick={() => setIsCardVisible(false)}
+            >
+              <IoIosClose size={24} />
+            </motion.div>
+
+            <Image
+              src="https://static-cdn.jtvnw.net/jtv_user_pictures/c5a9df33-4e9e-4df6-8dba-c3746041b95e-profile_image-300x300.png"
+              width={80}
+              height={80}
+              alt="Henry's Profile Image"
+              className="rounded-full"
+            />
+
+            <div className="flex flex-col items-center my-4 text-center">
+              <h1 className="text-2xl font-semibold ">Henry Gao</h1>
+              <p className="text-lime-400 text-xs">Auckland, New Zealand</p>
+            </div>
+
+            <p className="text-xs text-neutral-400 mb-4 text-center">
+              "Full-stack developer and retired gamer"
+            </p>
+
+            <div className="flex flex-col gap-2 w-full">
+              {links.map((link) => (
+                <motion.a
+                  target="_blank"
+                  href={link.link}
+                  rel="noopener noreferrer"
+                  key={link.text}
+                  className="bg-neutral-700 w-full text-center rounded-md py-1 text-sm font-semibold tracking-wide hover:cursor-pointer"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                >
+                  {link.text}
+                </motion.a>
+              ))}
+            </div>
+          </motion.div>
+        ) : (
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            key="button"
+            className="absolute text-white bottom-7 right-10 hover:cursor-pointer bg-neutral-800 px-6 py-1 rounded-xl tracking-wide font-semibold scale-125"
+            onClick={() => setIsCardVisible(true)}
+          >
+            Open
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
--- src/app/(frontend)/henry/page.tsx
+++ src/app/(frontend)/henry/page.tsx
@@ -3,8 +3,10 @@ import Card from './_components/Card'
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen bg-neutral-950">
-      <Card />
+    <div className="h-screen bg-neutral-950 p-12">
+      <div className="flex justify-center items-center relative w-full h-full bg-[url(/assets/babyme.jpg)] bg-cover bg-center rounded-3xl">
+        <Card />
+      </div>
     </div>
   )
 }
