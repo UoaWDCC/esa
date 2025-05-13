86577b8caaaeb4ac69e7517c6115bca99117530c
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-05-14 00:31:48 +1200

    creating base layout for homepage for devs to place components in

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,28 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
-
-import config from '@/payload.config'
-// import './styles.css'
+import 'src/styles/global.css';
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className='px-[20%]'>
+
+      {/* page content */}
+      <div className='w-full bg-amber-100'>
+        place landing component here and remove background colour
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
+
+      <div className='w-full bg-blue-100'>
+        place component 1 here and remove background colour
       </div>
+
+      <div className='w-full bg-red-100'>
+        place component 2 here and remove background colour
+      </div>
+
+      <div className='w-full bg-green-100'>
+        place component 3 here and remove background colour
+      </div>
+
     </div>
   )
 }
