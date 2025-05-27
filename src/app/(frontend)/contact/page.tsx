b0fa03272bcabf14bf989e6eef9defd5120f1446
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-05-27 23:39:24 +1200

    adding contact page template

---
+++ src/app/(frontend)/contact/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default async function ContactPage() {
+  return (
+    <div className="flex w-full">
+
+        {/* page content */}
+        <div className="w-full bg-amber-800">
+            place map component and remove background colour
+         </div>
+
+        <div className="w-full bg-blue-800">
+            place form component and remove background colour
+        </div>
+
+    </div>
+  )
+}
