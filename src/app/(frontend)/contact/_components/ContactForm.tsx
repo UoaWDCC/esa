a0dbae9f3f3fcfadb1ff2bf6f4517bce73307ec1
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-05-28 12:59:46 +1200

    added basic contact form

--- src/app/(frontend)/contact/_components/ContactForm.tsx
+++ src/app/(frontend)/contact/_components/ContactForm.tsx
@@ -4,6 +4,11 @@ import { useForm } from "react-hook-form";
 import { ContactInput, contactSchema } from "@/lib/zod/schema/contactInput";
 import {useState} from "react";
 import {zodResolver} from "@hookform/resolvers/zod";
+import {
+    Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
 
 export default function ContactForm() {
     const [sent, setSent] = useState(false);
@@ -23,8 +28,60 @@ export default function ContactForm() {
     }
 
     return (
-        <>
-        {/*TODO implement!!*/}
-        </>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6"
+            >
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your name"
+                                    {...field}
+                                    className="h-[35px] text-lg bg-primary-grey border border-white rounded-4xl text-primary-white placeholder:text-primary-white"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your email"
+                                    {...field}
+                                    className="h-[35px] text-lg bg-primary-grey border border-white rounded-4xl text-primary-white placeholder:text-primary-white"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Textarea
+                                    placeholder="Write your message"
+                                    className="min-h-[148px] text-lg bg-primary-grey border border-white rounded-xl text-primary-white placeholder:text-primary-white"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
     );
 }
