a8460984fc65ad4443fbd28dcb4c40af9575fe8b
Charles Leng <126838584+clen678@users.noreply.github.com>
2025-05-29 17:09:12 +1200

    Update Navbar.tsx
    added hover colour to text

--- src/app/(frontend)/_components/Navbar.tsx
+++ src/app/(frontend)/_components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
 
     // think of how you can make the width of the navbar scale with the screen size (% units ;o)
-    <nav className="mt-5 z-99 absolute top-0 left-1/2 transform -translate-x-1/2 bg-primary-black font-roboto-mono text-[1rem] text-primary-white flex justify-between items-center bg-primary-grey-light w-[1100px] px-28 rounded-full gap-4">
+    <nav className="mt-5 z-99 absolute top-0 left-1/2 transform -translate-x-1/2 bg-primary-black font-roboto-mono text-[1rem] text-primary-white hover:text-primary-grey flex justify-between items-center bg-primary-grey-light w-[1100px] px-28 rounded-full gap-4">
         <Link href="/news" className="" >NEWS</Link>
         <Link href="/contact" className="" >CONTACT</Link>
         <Link href="/" className='w-10 h-10 flex items-center justify-center'>
