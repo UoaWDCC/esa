a600d500c54f28c0e898dcd972c9875d35c5e503
Henry Gao <henrygao48@gmail.com>
2025-06-02 13:50:16 +1200

    Improve responsiveness for sponsors component to now work on mobile

--- src/app/(frontend)/_components/Sponsors.tsx
+++ src/app/(frontend)/_components/Sponsors.tsx
@@ -10,7 +10,7 @@ export default async function Sponsors() {
   const sponsors = parseSponsors(await getSponsors())
 
   return (
-    <div className="text-primary-white py-12">
+    <div className="py-12 text-primary-white">
       <div className="flex flex-col items-center gap-5 mb-12 ">
         <div className="relative">
           <Link
@@ -19,7 +19,7 @@ export default async function Sponsors() {
           >
             Our Sponsors
           </Link>
-          <div className="absolute flex items-center right-[-16.25rem] top-[-2.2rem] gap-1.5">
+          <div className="absolute items-center right-[-16.25rem] top-[-2.2rem] gap-1.5 hidden lg:flex">
             <Image
               width={90}
               height={90}
@@ -28,11 +28,11 @@ export default async function Sponsors() {
               unoptimized={true}
               className=""
             />
-            <p className="font-super-jellyfish translate-y-[-0.2rem] text-[1rem]">CLICK FOR MORE DETAILS!</p>
+            <p className="font-super-jellyfish translate-y-[-0.2rem]">CLICK FOR MORE DETAILS!</p>
           </div>
         </div>
 
-        <p className="w-[32rem] text-center text-xl/tight font-smeltex-medium tracking-[0.15em]">
+        <p className="w-[80%] text-center text-base/tight font-smeltex-medium tracking-[0.15em] lg:text-lg/tight lg:w-[28.5rem]">
           Enjoy discounts? As uni students we understand, that's why we've sponsored up for you.
           Take a look at ESA's sponsors!
         </p>
