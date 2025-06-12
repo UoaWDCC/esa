a3c976b097b4619c5123caa3f1d5a36167411f3a
Taks07 <yutakaoishi7@gmail.com>
2025-06-13 00:58:35 +1200

    feat: added image and scaling for mobile

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,19 +1,25 @@
 import ContactForm from "@/app/(frontend)/contact/_components/ContactForm";
+import Image from 'next/image'
 
 
 export default async function ContactPage() {
   return (
-    <div className="flex w-full lg:flex-row flex-col mt-[14vh] justify-around">
-
-        {/* page content */}
-        <div className="bg-amber-800">
-            place map component and remove background colour
-         </div>
-
-        <div className="">
-            <ContactForm />
+    <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-around lg:items-center mt-[13vh] lg:mt-[20vh] lg:px-40 gap-10">
+      <div className="w-full lg:w-[35%] flex flex-col items-center">
+        <div className="w-full aspect-[3/4] max-w-80 overflow-hidden rounded-[40px] relative">
+          <Image
+            src="/images/contact-us-image.png"
+            alt="Contact Us Image"
+            fill
+            className="object-cover"
+            priority
+          />
         </div>
-
+      </div>
+      
+      <div className="w-full lg:w-[60%]">
+        <ContactForm />
+      </div>
     </div>
   )
 }
