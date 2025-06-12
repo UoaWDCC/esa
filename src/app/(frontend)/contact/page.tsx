f4962ec3d498d35c4ba2edd928a10c3a0ebe515b
Taks07 <yutakaoishi7@gmail.com>
2025-06-13 01:09:22 +1200

    chore: played around even more with the spacing

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default async function ContactPage() {
   return (
-    <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-around lg:items-center mt-[13vh] lg:mt-[20vh] lg:px-30 gap-10">
+    <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-around lg:items-center mt-[15vh] lg:px-30 gap-10 lg:gap-0">
       <div className="w-full lg:w-[35%] flex flex-col items-center">
         <div className="w-full aspect-[3/4] max-w-80 overflow-hidden rounded-[40px] relative">
           <Image
