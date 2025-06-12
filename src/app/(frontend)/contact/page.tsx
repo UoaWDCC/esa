f0744451c18b90b16e2d54fb2470c14b61fbce1e
Taks07 <yutakaoishi7@gmail.com>
2025-06-12 19:57:11 +1200

    feat: styled most of the contact form

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -3,14 +3,14 @@ import ContactForm from "@/app/(frontend)/contact/_components/ContactForm";
 
 export default async function ContactPage() {
   return (
-    <div className="flex w-full lg:flex-row flex-col">
+    <div className="flex w-full lg:flex-row flex-col mt-[14vh] justify-around">
 
         {/* page content */}
-        <div className="w-full bg-amber-800">
+        <div className="bg-amber-800">
             place map component and remove background colour
          </div>
 
-        <div className="w-full bg-blue-500">
+        <div className="">
             <ContactForm />
         </div>
 
