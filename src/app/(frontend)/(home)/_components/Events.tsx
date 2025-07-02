7030392a2d1db94eda59bdab664bbbba71d9974f
Kieran McKenna <kmck133@aucklanduni.ac.nz>
2025-07-03 02:50:06 +1200

    Event cards and title frames aligned properly

--- src/app/(frontend)/(home)/_components/Events.tsx
+++ src/app/(frontend)/(home)/_components/Events.tsx
@@ -11,26 +11,34 @@ export default function Events() {
   return (
     <section className="px-6 md:px-16 pb-20 text-white h-[1043px]">
       {/* page title */}
-      <div className="relative">
+      <div className="relative w-fit mx-auto mt-14">
         <Image
           src="/assets/events_title.png"
           alt="Events"
           width={456}
           height={116}
-          className="mx-auto mt-14 object-contain object-top"
+          className="object-contain object-top"
         />
 
+        <Image
+          src="/assets/frame.png"
+          alt="top left frame"
+          width={60}
+          height={60}
+          className="absolute -top-5 -left-14 scale-x-[-1] scale-y-[-1] w-[20%] md:w-[15%] -rotate-18"
+        />
         <Image
           src="/assets/frame.png"
           alt="bottom right frame"
           width={60}
           height={60}
-          className="absolute -bottom-10 -right-1 w-[12%] md:w-[7.5%]"
+          className="absolute -bottom-4 -right-10 w-[20%] md:w-[15%]"
         />
       </div>
+      
 
       {/* upcoming slots */}
-      <div className="mt-28 space-y-12">
+      <div className="mt-[52px] space-y-[46px] flex flex-col items-center">
         <EventCard
           bgSrc="/assets/latest_strip.png"
           title="Coming soon"
@@ -51,19 +59,21 @@ export default function Events() {
 
       {/* latest header */}
       <div className="flex flex-col items-center">
-        <div className="mt-20 mb-4 bg-[#871F1B] text-white text-2xl font-reservoir-grunge px-4.5 py-2 rounded-2xl inline-block">
+        <div className="mt-[81px] mb-4 bg-[#871F1B] text-white text-2xl font-reservoir-grunge px-4.5 py-2 rounded-2xl inline-block">
           Latest&nbsp;Event:
         </div>
+
+        {/* event card */}
+        <EventCard
+          bgSrc="/assets/latest_strip.png"
+          title=""
+          month="May"
+          day="10"
+        />
       </div>
       
 
-      {/* event card */}
-      <EventCard
-        bgSrc="/assets/latest_strip.png"
-        title=""
-        month="May"
-        day="10"
-      />
+      
     </section>
   )
 }
