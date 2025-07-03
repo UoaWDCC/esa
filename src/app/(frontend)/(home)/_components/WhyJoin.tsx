a3becdb643435e8de61f34dabdf43496294842a2
NoobCrewDelux <j0sephw1532@gmail.com>
2025-07-03 17:11:57 +1200

    Made desktop compatible, fixed content y margin scaling by viewport width rather than height

--- src/app/(frontend)/(home)/_components/WhyJoin.tsx
+++ src/app/(frontend)/(home)/_components/WhyJoin.tsx
@@ -15,29 +15,38 @@ const WhyJoin = () => {
         src="/images/home/HeroPageImage.jpg"
         alt="Why Join Background"
         fill
-        className="absolute origin-center object-cover scale-180 brightness-30 "
+        className="absolute origin-center object-cover scale-180 brightness-20 lg:scale-140"
       />
       {/* Content overlay */}
-      <div id="content" className="relative my-[20vw] mx-[10vw] w-[80vw]">
+      <div id="content" className="relative z-10 my-[15vh] mx-[10vw] w-[80vw] lg:mb-[25vh]">
         {/* Heading SVG */}
-        <Image src="WhyJoinESA.svg" alt="Heading Decoration" width="616" height="196" />
+        <Image
+          src="WhyJoinESA.svg"
+          alt="WhyJoinESA svg"
+          width="616"
+          height="196"
+          className="origin-bottom-left lg:scale-90"
+        />
         {/* Body Text One*/}
-        <div id="body-text-one" className="mt-[6vh]">
-          <p className="text-white text-lg font-smeltex-medium">
+        <div id="body-text-one" className="mt-[6vh] lg:w-[50vw] max-w-[1000px]">
+          <p className="text-white text-lg lg:text-xl">
             We&apos;re doing things our way - fresh events, fun people, and a vibe that feels like
             your favourite hangout spot. Kinda new, kinda nostalgic, always a good time.
           </p>
         </div>
         {/* Body Text Two*/}
-        <div id="body-text-two" className="mt-[3vh]">
-          <p className="text-white text-lg font-smeltex-medium">
+        <div id="body-text-two" className="mt-[3vh] lg:w-[50vw] max-w-[1000px]">
+          <p className="text-white text-lg lg:text-xl ">
             Lorem ipsum dolor sit amet consecutetur. Elementum gravida egestas id consecutetur
             volutpat id nec consecutetur. Pretium pellentesque a consecutetur urna
           </p>
         </div>
 
         {/* Join button */}
-        <div id="join-button" className="flex flex-col items-center mx-auto mt-[4.5vh]">
+        <div
+          id="join-button"
+          className="flex flex-col items-center lg:items-start mx-auto mt-[4.5vh]"
+        >
           <Button href="/signup" className="px-8 py-3.25">
             Join the ESA Family!
           </Button>
@@ -49,7 +58,7 @@ const WhyJoin = () => {
         alt="Film Strip"
         width="758"
         height="1072"
-        className="absolute hidden 2xl:block right-0 -top-[29px] origin-top-right"
+        className="absolute hidden lg:block right-0 -top-[29px] origin-top-right"
       />
     </div>
   )
