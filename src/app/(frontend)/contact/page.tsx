b50f07e3b14b99a764caad9f24eb87b1635f01fb
Daniel Chen <dc17088@thameshigh.school.nz>
2025-07-14 15:54:15 +1200

    somewhat working new foundation

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,21 +1,26 @@
 import ContactForm from "@/app/(frontend)/contact/_components/ContactForm";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default async function ContactPage() {
   return (
-    <div className="w-full min-h-screen flex flex-col md:flex-row justify-center lg:justify-around lg:items-center mt-[15vh] px-10 mb-6 lg:px-30 gap-10 lg:gap-0">
-      <div className="relative w-full lg:w-[45%] h-[420px] lg:h-[600px] flex items-center justify-center">
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-dvh pt-[15vh] pb-10 px-6">
+      
+      {/* Image container */}
+      <div className="relative w-full max-w-[400px] mb-8 md:mb-0 md:mr-12 md:aspect-[3/4]">
         <Image
           src="/images/contact-us-image.png"
           alt="contact us image"
           fill
           className="object-cover rounded-2xl"
+          priority
         />
       </div>
 
-      <div className="w-full lg:w-[60%]">
+      {/* Form container */}
+      <div className="w-full max-w-xl">
         <ContactForm />
       </div>
+
     </div>
   );
 }
