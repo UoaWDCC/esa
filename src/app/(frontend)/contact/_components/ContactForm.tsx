ac23ede8fe9f1618dc51b2a4eb04ba768453ba8a
Daniel Chen <dc17088@thameshigh.school.nz>
2025-07-14 14:55:49 +1200

    new sizing completed / details added

--- src/app/(frontend)/contact/_components/ContactForm.tsx
+++ src/app/(frontend)/contact/_components/ContactForm.tsx
@@ -7,73 +7,74 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import ArrowUp from "@/components/icons/ArrowUp";
 import FormInput from "@/components/ui/FormInput";
 import FormTextarea from "@/components/ui/FormTextArea";
-import {Button} from "@/components/ui/Button";
+import { Button } from "@/components/ui/Button";
 
 export default function ContactForm() {
-    const [sent, setSent] = useState(false);
+  const [sent, setSent] = useState(false);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-    } = useForm<ContactInput>({
-        resolver: zodResolver(contactSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ContactInput>({
+    resolver: zodResolver(contactSchema),
+  });
 
-    async function onSubmit(values: ContactInput) {
-        console.log(values);
-    }
+  async function onSubmit(values: ContactInput) {
+    console.log(values);
+  }
 
-    return (
-        <div className="w-full mx-auto text-center text-primary-white bg-transparent flex flex-col items-center">
-            <h3 className="underline underline-offset-4 mb-2">
-                Let’s have a chat!
-            </h3>
-            <p className="mb-6 text-xs max-w-[60%]">
-                We’d love to hear from you! You can contact us with the
-                form below or send us an email or a call!
-            </p>
+  return (
+    <div className="w-full mx-auto text-center text-primary-white bg-transparent flex flex-col items-center">
+      <h3 className="underline underline-offset-4 mb-2">
+        Let’s have a chat!
+      </h3>
+      <p className="mb-6 text-xs max-w-[60%]">
+        We’d love to hear from you! You can contact us with the
+        form below or send us an email or a call!
+      </p>
 
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 w-full max-w-lg px-5">
-                <FormInput
-                    placeholder="Enter your name"
-                    {...register("name")}
-                    error={errors.name}
-                    className="w-full"
-                />
-                <FormInput
-                    placeholder="Enter your email"
-                    {...register("email")}
-                    error={errors.email}
-                    className="w-full"
-                />
-                <FormTextarea
-                    placeholder="Write your message"
-                    {...register("message")}
-                    error={errors.message}
-                    className="w-full"
-                />
-                <Button type="submit" disabled={isSubmitting} className="flex flex-row items-center gap-x-2">
-                    {isSubmitting ? "Submitting..." : (
-                        <>
-                            Send
-                            <ArrowUp className="size-3" />
-                        </>
-                    )}
-
-                </Button>
-            </form>
-
-            <div className="mt-7 px-5 text-xs text-left w-full max-w-lg">
-                <div className="flex px-3 border-b border-primary-white justify-between">
-                    <span className="max-w-lg">Email</span>
-                    <span className="max-w-lg">sdkfj@asadljcf.com</span>
-                </div>
-                <div className="flex px-3 border-b border-primary-white justify-between mt-2">
-                    <span className="max-w-lg">Contact number</span>
-                    <span className="max-w-lg">90812 2134</span>
-                </div>
-            </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 w-full max-w-lg px-5">
+        <FormInput
+          placeholder="Enter your name"
+          {...register("name")}
+          error={errors.name}
+          className="w-full"
+        />
+        <FormInput
+          placeholder="Enter your email"
+          {...register("email")}
+          error={errors.email}
+          className="w-full"
+        />
+        <FormTextarea
+          placeholder="Write your message"
+          {...register("message")}
+          error={errors.message}
+          className="w-full"
+        />
+        <div className="w-full flex justify-end">
+          <Button type="submit" disabled={isSubmitting} className="flex flex-row items-center gap-x-2">
+            {isSubmitting ? "Submitting..." : (
+              <>
+                Send
+                <ArrowUp className="size-3" />
+              </>
+            )}
+          </Button>
         </div>
-    );
+      </form>
+
+      <div className="mt-7 px-5 text-xs text-left w-full max-w-lg">
+        <div className="flex px-3 border-b border-primary-white justify-between">
+          <span className="max-w-lg">Email</span>
+          <span className="max-w-lg">uoaesa@gmail.com</span>
+        </div>
+        <div className="flex px-3 border-b border-primary-white justify-between mt-2">
+          <span className="max-w-lg">Contact number</span>
+          <span className="max-w-lg">020 4180 2771</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -1,23 +1,21 @@
 import ContactForm from "@/app/(frontend)/contact/_components/ContactForm";
-import Image from 'next/image'
-
+import Image from 'next/image';
 
 export default async function ContactPage() {
   return (
-    <div className="w-full flex flex-col md:flex-row justify-center lg:justify-around lg:items-center mt-[15vh] px-10 mb-6 lg:px-30 gap-10 lg:gap-0">
-      <div className="relative w-full lg:w-[35%] flex items-center justify-center">
-          <Image
-              src="/images/contact-us-image.png"
-              alt="contact us image"
-              height={600}
-              width={400}
-              className="object-cover rounded-2xl"
-          />
+    <div className="w-full min-h-screen flex flex-col md:flex-row justify-center lg:justify-around lg:items-center mt-[15vh] px-10 mb-6 lg:px-30 gap-10 lg:gap-0">
+      <div className="relative w-full lg:w-[45%] h-[420px] lg:h-[600px] flex items-center justify-center">
+        <Image
+          src="/images/contact-us-image.png"
+          alt="contact us image"
+          fill
+          className="object-cover rounded-2xl"
+        />
       </div>
 
       <div className="w-full lg:w-[60%]">
         <ContactForm />
       </div>
     </div>
-  )
+  );
 }
