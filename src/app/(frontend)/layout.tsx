af700f97fc4b2c3e5732e85311e7b8acae666d4b
Charles Leng <126838584+clen678@users.noreply.github.com>
2025-07-17 00:28:42 +1200

    Website SEO and sitemap + robots generation (#109)
    * installing next-sitemap to autogenerate sitemax on build, adding metadata and alt tags for seo
    
    * Update Hero.tsx
    
    changing ESA to Eastern Students Association
    
    * Update robots.txt
    
    disallow admin
    
    * Update robots.txt again
    
    disallow /test

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,9 +3,39 @@ import 'src/styles/global.css';
 import Footer from '../../components/navigation/footer';
 import Navbar from '../../components/navigation/Navbar';
 
+// SEO metadata
 export const metadata = {
-    description: 'A blank template using Payload in a Next.js app.',
-    title: 'Payload Blank Template',
+    description: "Connect, compete, and make lasting memories with the Eastern Students Association. We're a social club bringing students together through events, sports, and campus life at the University of Auckland.",
+    title: "Eastern Students Association",
+    keywords: [
+        'Eastern Students Association',
+        'ESA Club',
+        'University of Auckland',
+        'student life',
+        'uni events',
+        'Auckland clubs',
+        'uni sports',
+        'student social club',
+      ],
+      openGraph: {
+        title: "Eastern Students Association",
+        description: "Connect, compete, and make lasting memories with the Eastern Students Association. We're a social club bringing students together through events, sports, and campus life at the University of Auckland.",
+        url: "https://esa.wdcc.co.nz",
+        siteName: "Eastern Students Association",
+        images: [
+          {
+            url: "https://esa.wdcc.co.nz/images/logo/esa_logo.png",
+            width: 261,
+            height: 261,
+            alt: "Eastern Students Association logo",
+          },
+        ],
+        locale: "en_NZ",
+        type: "website",
+      },
+      icons: {
+        icon: '/images/logo/esa_logo.png',
+      },
 };
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
