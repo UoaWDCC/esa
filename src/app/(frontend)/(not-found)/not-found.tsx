23f68e6ee838f91950d0b247ab99dac7a7f5dd23
Kieran McKenna <160551320+kmck133@users.noreply.github.com>
2025-07-17 10:30:37 +1200

    Added new 404 page (#102)
    * Added new 404 page
    
    * changed div for line in 404 page to hr

--- src/app/(frontend)/(not-found)/not-found.tsx
+++ src/app/(frontend)/(not-found)/not-found.tsx
@@ -1,22 +1,29 @@
 import Link from 'next/link';
+import FrogBackground from '@/components/backgrounds/FrogBackground';
 
 export default function notFound() {
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen bg-zinc-900">
-            <h2 className="text-primary-red p-10">404 - Page Not Found</h2>
+        <div className="flex flex-col min-h-screen items-center justify-center text-center px-4">
+            <FrogBackground />
+            
+            {/* Title/body text */}
+            <h3 className="text-primary-red p-2"> 404 - Page Not Found </h3>
+            
+            <div className="w-full mx-auto text-center text-primary-white flex flex-col items-center tracking-widest">
+                <p className="text-primary-white mb-2">
+                    Oh no!
+                </p>
+                <p className="text-primary-white mb-1">
+                    Looks like the page you&apos;re looking for doesn&apos;t exist.
+                </p>
+            </div>
+
+            <hr className="w-64 h-px bg-white opacity-50 my-4"></hr>
 
             <div className="flex space-x-6 text-white">
-                {' '}
-                {/* Links to other pages for navigation */}
-                <Link href="/public" className="hover:underline">
-                    Home Page
-                </Link>
-                <Link href="/public" className="hover:underline">
-                    About Us
-                </Link>
-                <Link href="/contact" className="hover:underline">
-                    Contact Us
-                </Link>
+                <Link href="/" className="hover:underline">Go Home</Link>
+                <Link href="/about-us" className="hover:underline">About Us</Link>
+                <Link href="/contact" className="hover:underline">Contact Us</Link>
             </div>
         </div>
     );
