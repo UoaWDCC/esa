cab9e2dbba0512bca9d14fc53d79b6940bbb8634
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-07-19 01:03:16 +1200

    hero page fixes

--- src/app/(frontend)/(home)/_components/Hero.tsx
+++ src/app/(frontend)/(home)/_components/Hero.tsx
@@ -5,15 +5,15 @@ import {Button} from "@/components/ui/Button";
 
 const Hero = () => {
   return (
-    <div className="min-h-screen relative overflow-hidden">
+    <div className="h-[700px] md:min-h-screen relative overflow-hidden">
       {/* Black Background */}
       <div className="absolute inset-0"></div>
 
       {/* Background Image covering full screen with zoom */}
-      <div className="absolute inset-0 h-screen bg-cover bg-center bg-no-repeat bg-[url('/images/home/HeroPageImage.png')] transform scale-100 transition-transform duration-500 ease-in-out">
+      <div className="absolute inset-0 h-[700px] md:h-screen bg-cover bg-center bg-no-repeat bg-[url('/images/home/HeroPageImage.png')] transform scale-100 transition-transform duration-500 ease-in-out">
 
           {/* Hero Section */}
-          <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 md:px-8 xl:px-20 text-center">
+          <div className="relative z-10 flex flex-col items-center justify-center h-[700px] md:min-h-screen px-4 md:px-8 xl:px-20 text-center">
             <Image
               className="w-full h-auto hidden md:block"
               src="/images/HeroTitle.svg"
