d3a9b651802cb9e6dea4c378389595247378becc
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-07-19 22:48:54 +1200

    adding animation to navbar, fixing svg

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -32,40 +34,18 @@ export default function Navbar() {
                 </Link>
             </nav>
 
-            <nav className="md:hidden fixed top-0 left-0 right-0 z-50">
+            <nav className="md:hidden fixed top-5 left-0 right-0 z-50">
                 <div className="flex justify-between items-center px-4 py-3">
                     <button
                         onClick={toggleMenu}
-                        className="w-20 h-20 rounded-full flex items-center justify-center text-primary-white hover:text-primary-grey transition-colors hover:cursor-pointer bg-primary-grey-light"
+                        className="w-18 h-18 rounded-full flex items-center justify-center text-primary-white hover:text-primary-grey transition-colors hover:cursor-pointer bg-primary-grey-light"
                         aria-label="Toggle menu"
                     >
-                        <svg
-                            className="w-8 h-8"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                        >
-                            {isOpen ? (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            ) : (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            )}
-                        </svg>
+                      <Menu />
                     </button>
                     <Link
                         href="/"
-                        className="w-20 h-20 rounded-full flex items-center justify-center"
-                        style={{ backgroundColor: '#404040' }}
+                        className="w-18 h-18 rounded-full flex items-center justify-center bg-primary-grey-light"
                     >
                         <Image
                             src="/images/logo/esa_logo.png"
@@ -74,44 +54,57 @@ export default function Navbar() {
                             width={66}
                         />
                     </Link>
-                    <div className="w-20"></div>
+                    <div className="w-18"></div>
                 </div>
 
-                {isOpen && (
-                    <div
-                        className="absolute top-full left-4 w-40 rounded-lg mt-2 bg-primary-grey-light">
-                        <div className="flex flex-col py-4 px-4 space-y-3 font-roboto-mono text-primary-white">
-                            <Link
-                                href="/news"
-                                className="hover:text-primary-grey transition-colors py-1 px-1"
-                                onClick={() => setIsOpen(false)}
+                <AnimatePresence>
+                  {isOpen && (
+                      <motion.div
+                          initial={{ opacity: 0, x: -20 }}
+                          animate={{ opacity: 1, x: 1 }}
+                          exit={{ opacity: 0, x: -20 }}
+                          transition={{ duration: 0.2 }}
+                          className="absolute top-1 left-[12px] w-40 rounded-lg mt-2 bg-primary-grey-light">
+                            <button
+                                onClick={toggleMenu}
+                                className="w-18 h-18 rounded-full flex items-center justify-center text-primary-white hover:text-primary-grey transition-colors hover:cursor-pointer bg-primary-grey-light"
+                                aria-label="Toggle menu"
                             >
-                                NEWS
-                            </Link>
-                            <Link
-                                href="/contact"
-                                className="hover:text-primary-grey transition-colors py-1 px-1"
-                                onClick={() => setIsOpen(false)}
-                            >
-                                CONTACT
-                            </Link>
-                            <Link
-                                href="/events"
-                                className="hover:text-primary-grey transition-colors py-1 px-1"
-                                onClick={() => setIsOpen(false)}
-                            >
-                                EVENTS
-                            </Link>
-                            <Link
-                                href="/about-us"
-                                className="hover:text-primary-grey transition-colors py-1 px-1"
-                                onClick={() => setIsOpen(false)}
-                            >
-                                ABOUT US
-                            </Link>
-                        </div>
-                    </div>
-                )}
+                              <X className={"z-50"}/>
+                            </button>
+                            <div className="flex flex-col pb-4  px-4 space-y-3 font-roboto-mono text-primary-white">
+                              <Link
+                                  href="/news"
+                                  className="hover:text-primary-grey transition-colors py-1 px-1"
+                                  onClick={() => setIsOpen(false)}
+                              >
+                                  NEWS
+                              </Link>
+                              <Link
+                                  href="/contact"
+                                  className="hover:text-primary-grey transition-colors py-1 px-1"
+                                  onClick={() => setIsOpen(false)}
+                              >
+                                  CONTACT
+                              </Link>
+                              <Link
+                                  href="/events"
+                                  className="hover:text-primary-grey transition-colors py-1 px-1"
+                                  onClick={() => setIsOpen(false)}
+                              >
+                                  EVENTS
+                              </Link>
+                              <Link
+                                  href="/about-us"
+                                  className="hover:text-primary-grey transition-colors py-1 px-1"
+                                  onClick={() => setIsOpen(false)}
+                              >
+                                  ABOUT US
+                              </Link>
+                          </div>
+                      </motion.div>
+                  )}
+                </AnimatePresence>
             </nav>
         </>
     );
