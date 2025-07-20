39508fad06916965b91c965dc1f9ed2d286e14aa
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-07-20 17:32:59 +1200

    adding margin bottom

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactForm from '@/app/(frontend)/contact/_components/ContactForm';
 export default function ContactPage() {
     return (
         <section className="mx-auto mt-16 flex flex-col md:flex-row items-center justify-center h-dvh px-5">
-            <div className="w-auto md:mb-0 md:mr-8 md:w-auto max-w-md">
+            <div className="mb-8 w-auto md:mb-0 md:mr-8 md:w-auto max-w-md">
                 <Image
                     src="/images/contact-us-image.png"
                     alt="Contact us image"
