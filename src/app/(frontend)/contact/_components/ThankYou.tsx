68e09629f77747792566b8f3f6a502e4de00c336
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-07-20 23:23:32 +1200

    removed text-xs

--- src/app/(frontend)/contact/_components/ThankYou.tsx
+++ src/app/(frontend)/contact/_components/ThankYou.tsx
@@ -1,6 +1,6 @@
 export default function ThankYou() {
     return (
-        <div className="text-primary-white font-normal text-xs">
+        <div className="text-primary-white font-normal">
             <p className="mb-2 mt-10 h-4">Thank you!</p>
             <p className="mb-68">Your form has been sent and we will get back to you shortly.</p>
         </div>
