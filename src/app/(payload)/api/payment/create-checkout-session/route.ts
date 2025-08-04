d8dd1ab0e2b1245a33aaeb4369d022664c78437a
Henry Gao <henrygao48@gmail.com>
2025-08-04 17:30:58 +1200

    Replace all process.env references with env object schema + Validation of schema on build

--- src/app/(payload)/api/payment/create-checkout-session/route.ts
+++ src/app/(payload)/api/payment/create-checkout-session/route.ts
@@ -1,45 +1,48 @@
-import Stripe from 'stripe'
+import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
-})
+// T3 env
+import { env } from 'config/serverEnv';
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2025-06-30.basil',
+});
 
 export async function POST(req: Request) {
-  try {
-    const signupData = await req.json()
+    try {
+        const signupData = await req.json();
 
-    // Hopefully this works on deployment. If redirects don't work, we can use a different method to get the origin.
-    const origin = process.env.NEXT_PUBLIC_BASE_URL || 'https://esa.wdcc.co.nz'
+        // Hopefully this works on deployment. If redirects don't work, we can use a different method to get the origin.
+        const origin = env.BASE_URL || 'https://esa.wdcc.co.nz';
 
-    const session = await stripe.checkout.sessions.create({
-      mode: 'payment',
-      line_items: [
-        {
-          price_data: {
-            currency: 'nzd',
-            product_data: {
-              name: 'ESA Year Membership',
+        const session = await stripe.checkout.sessions.create({
+            mode: 'payment',
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'nzd',
+                        product_data: {
+                            name: 'ESA Year Membership',
+                        },
+                        unit_amount: 600, // NZD 6.00.
+                    },
+                    quantity: 1,
+                },
+            ],
+            payment_intent_data: {
+                metadata: {
+                    ...signupData,
+                },
             },
-            unit_amount: 600, // NZD 6.00.
-          },
-          quantity: 1,
-        },
-      ],
-      payment_intent_data: {
-        metadata: {
-          ...signupData,
-        },
-      },
-      customer_email: signupData.email,
-      success_url: `${origin}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${origin}/signup`,
-    })
+            customer_email: signupData.email,
+            success_url: `${origin}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${origin}/signup`,
+        });
 
-    return Response.json({ url: session.url })
-  } catch (error: any) {
-    console.error(error)
-    return new Response(JSON.stringify({ message: 'Stripe session creation failed.' }), {
-      status: 500,
-    })
-  }
+        return Response.json({ url: session.url });
+    } catch (error: any) {
+        console.error(error);
+        return new Response(JSON.stringify({ message: 'Stripe session creation failed.' }), {
+            status: 500,
+        });
+    }
 }
