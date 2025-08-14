8c73b72b684400134d81b9433e0c7039e02145cb
Maternus Kuang <maternuskuang@gmail.com>
2025-08-14 23:10:39 +1200

    Not sure if completetd

--- src/app/(frontend)/(home)/_components/Sponsors.tsx
+++ src/app/(frontend)/(home)/_components/Sponsors.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import Carousel from './Carousel'
-import Link from 'next/link'
+import React from 'react';
+import Carousel from './Carousel';
+import Link from 'next/link';
+import { useSponsors } from '@/features/sponsors/data/tanstack/useSponsors';
+import Image from 'next/image';
+import Title from '@/components/ui/Title';
+import parseSponsors from '@/features/sponsors/data/payload/parseSponsors';
+import { SponsorData } from '@/types/SponsorData';
 
-import { getSponsors } from '@/features/sponsors/data/payload/getSponsors'
-import parseSponsors from '@/features/sponsors/data/payload/parseSponsors'
-import Image from 'next/image'
-import Title from "@/components/ui/Title";
+export default function Sponsors() {
+    const { data } = useSponsors();
+    const parsedSponsors = (data ? parseSponsors(data) : []) as SponsorData[];
+    if (!parsedSponsors.length) return null;
 
-export default async function Sponsors() {
-  const sponsors = parseSponsors(await getSponsors())
+    return (
+        <div className="py-12 text-primary-white">
+            <div className="flex flex-col items-center gap-5 mb-12 ">
+                <div className="relative">
+                    <Link href="/sponsors">
+                        <Title className="tracking-wider">Our Sponsors</Title>
+                    </Link>
+                    <div className="absolute items-center right-[-16.25rem] top-[-2.2rem] gap-1.5 hidden lg:flex">
+                        <Image
+                            width={90}
+                            height={90}
+                            alt="Arrow pointing to sponsors"
+                            src="/SponsorsArrow.svg"
+                            unoptimized={true}
+                            className=""
+                        />
+                        <p className="font-super-jellyfish translate-y-[-0.2rem]">
+                            CLICK FOR MORE DETAILS!
+                        </p>
+                    </div>
+                </div>
 
-  return (
-    <div className="py-12 text-primary-white">
-      <div className="flex flex-col items-center gap-5 mb-12 ">
-        <div className="relative">
-          <Link
-            href="/sponsors"
-          >
-            <Title className="tracking-wider">
-              Our Sponsors
-            </Title>
-          </Link>
-          <div className="absolute items-center right-[-16.25rem] top-[-2.2rem] gap-1.5 hidden lg:flex">
-            <Image
-              width={90}
-              height={90}
-              alt="Arrow pointing to sponsors"
-              src="/SponsorsArrow.svg"
-              unoptimized={true}
-              className=""
-            />
-            <p className="font-super-jellyfish translate-y-[-0.2rem]">CLICK FOR MORE DETAILS!</p>
-          </div>
+                <p className="w-[80%] text-center text-base/tight font-smeltex-medium tracking-[0.15em] lg:text-lg/tight lg:w-[28.5rem]">
+                    Enjoy discounts? As uni students we understand, that&apos;s why we&apos;ve
+                    sponsored up for you. Take a look at ESA&apos;s sponsors!
+                </p>
+            </div>
+            <Carousel sponsors={parsedSponsors ?? []} />
         </div>
-
-        <p className="w-[80%] text-center text-base/tight font-smeltex-medium tracking-[0.15em] lg:text-lg/tight lg:w-[28.5rem]">
-          Enjoy discounts? As uni students we understand, that&apos;s why we&apos;ve sponsored up for you.
-          Take a look at ESA&apos;s sponsors!
-        </p>
-      </div>
-      <Carousel sponsors={sponsors ?? []} />
-    </div>
-  )
+    );
 }
