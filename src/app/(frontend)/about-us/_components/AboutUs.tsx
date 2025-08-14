c7ca0b30ae80de3d7f0a24253f25022a4c7339ab
Henry Gao <hgao080@aucklanduni.ac.nz>
2025-08-14 13:10:25 +1200

    Use next images for backgrounds for optimisations (#152)
    * Use next images for backgrounds for optimisations
    
    * fixed some code
    
    ---------
    
    Co-authored-by: owenli <102705391+VoxelFS@users.noreply.github.com>

--- src/app/(frontend)/about-us/_components/AboutUs.tsx
+++ src/app/(frontend)/about-us/_components/AboutUs.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
-import React from 'react';
 
 export default function AboutUs() {
     return (
-        <div className="flex items-center justify-center bg-[url(/images/aboutus/AboutUsImage.jpg)] bg-cover bg-no-repeat bg-center min-h-screen relative px-6 md:px-0">
-            <div className="flex items-center gap-2">
+        <div className="flex items-center justify-center min-h-screen relative px-6 md:px-0 overflow-hidden">
+            <Image
+                src="/images/aboutus/AboutUsImage.jpg"
+                alt="about us"
+                fill
+                priority
+                className="object-cover object-center"
+            />
+
+            <div className="flex items-center gap-2 relative z-10">
                 {/* About Us Text */}
                 <div className="flex flex-col text-center md:text-start md:w-[28rem]">
                     <h3>ABOUT US</h3>
