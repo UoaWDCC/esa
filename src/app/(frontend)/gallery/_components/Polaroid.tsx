bde53c2dcd3c0f2cd71da8b858219e39785d4c53
Taks07 <yutakaoishi7@gmail.com>
2025-08-30 10:21:09 +1200

    feat: created PolaroidProps interface exported by Polaroid component

---
+++ src/app/(frontend)/gallery/_components/Polaroid.tsx
@@ -0,0 +1,12 @@
+
+export interface PolaroidProps {
+    image: string;
+    eventDate: string;
+    eventName: string;
+    pinColour: string;
+    variation: 'small' | 'large'; // TODO: Revisit this
+}
+
+export default function Polaroid() {
+
+}
