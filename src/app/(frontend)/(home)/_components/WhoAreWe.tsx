5fcb0a5c68e762070e84fa6bd9f216819bf27cad
Joseph <j0sephw1532@gmail.com>
2025-08-29 22:50:39 +1200

    Finished for now, just needs font updated

--- src/app/(frontend)/(home)/_components/WhoAreWe.tsx
+++ src/app/(frontend)/(home)/_components/WhoAreWe.tsx
@@ -17,22 +17,6 @@ export default function WhoAreWe() {
         <div className="w-full flex justify-center md:w-auto">
             <div className="relative w-full">
                 {/* bottom right frame*/}
-                <Image
-                    src="/images/home/frame.png"
-                    alt="top left frame"
-                    width={60}
-                    height={60}
-                    className="absolute -bottom-10 -right-4 min-[490px]:-bottom-18 md:-bottom-10 w-[12%] md:w-[7.5%]"
-                />
-
-                {/* top left frame */}
-                <Image
-                    src="/images/home/frame.png"
-                    alt="bottom right frame"
-                    width={60}
-                    height={60}
-                    className="absolute -top-5 -left-9 min-[490px]:-top-14 md:-top-5 scale-x-[-1] scale-y-[-1] w-[12%] md:w-[7.5%] -rotate-14 md:-rotate-18"
-                />
 
                 {/* main box component */}
                 <div className="flex items-center justify-center gap-11 py-9 px-10 rounded-[3.5em] md:rounded-[5em] bg-[#ebe9e6]">
@@ -52,10 +36,12 @@ export default function WhoAreWe() {
                     </div>
 
                     {/* Right side text */}
-                    <div className="z-10 flex flex-col items-center gap-4">
+                    <div className="z-10 flex flex-col items-center gap-4 mt-4">
                         <div className="relative">
                             {/* title */}
-                            <Title className="bg-primary-red-400">Who are we?</Title>
+                            <Title className="bg-primary-red-400 rounded-3xl p-5">
+                                Who are we?
+                            </Title>
                             {/* arrow */}
                             <Image
                                 src="/images/home/arrow.png"
@@ -64,28 +50,71 @@ export default function WhoAreWe() {
                                 height={105}
                                 className="absolute top-[2rem] left-[-6.2rem] hidden md:block w-[33%]"
                             />
+                            <Image
+                                src="/images/home/arrow3.svg"
+                                alt="arrow3.svg"
+                                width={39}
+                                height={100}
+                                className="absolute top-[2rem] left-[-3.4rem]"
+                            />
                         </div>
 
                         {/* description */}
-                        <p className="w-[88%] md:w-56 mb-4 font-medium text-center text-black text-base/tight bg">
+                        <div className="w-[88%] max-w-[265px] md:max-w-none md:w-56 mb-2 font-medium text-center text-black text-base/tight bg">
                             ESA Social Club is your go-to community for fun, connection and a little
                             friendly competition.
-                        </p>
-                        <div className="md:hidden relative w-90 flex justify-center px-4">
+                        </div>
+
+                        <div className="md:hidden relative w-90 flex justify-center pl-3">
                             <Image
                                 src="/images/home/who_are_we_mobile.png"
                                 alt="team photo"
-                                width={100}
+                                width={120}
                                 height={100}
                                 className="w-[90%] h-auto max-w-full object-contain"
                             />
-                            <div className="flex flex-col">
+
+                            <div className="flex flex-col justify-center">
+                                <div className="relative">
+                                    <Image
+                                        src="\images\home\frame_tr_small.svg"
+                                        alt="team_esa.svg"
+                                        width={5}
+                                        height={20}
+                                        className="absolute right-0 top-[-7px]"
+                                    />
+                                    <Image
+                                        src="\images\home\frame_tr_large.svg"
+                                        alt="team_esa.svg"
+                                        width={13}
+                                        height={20}
+                                        className="absolute right-[-12px] top-[-2px]"
+                                    />
+                                </div>
+                                {/* The ESA Team */}
                                 <Image
                                     src="\images\home\team_esa.svg"
                                     alt="team_esa.svg"
                                     width={131}
-                                    height={150}
+                                    height={100}
                                 />
+                                {/* particles bottom left */}
+                                <div className="relative">
+                                    <Image
+                                        src="\images\home\frame_tr_small.svg"
+                                        alt="team_esa.svg"
+                                        width={5}
+                                        height={20}
+                                        className="absolute left-0 bottom-[-8px] rotate-190"
+                                    />
+                                    <Image
+                                        src="\images\home\frame_tr_large.svg"
+                                        alt="team_esa.svg"
+                                        width={13}
+                                        height={20}
+                                        className="absolute left-[-12px] bottom-[-2px] rotate-200"
+                                    />
+                                </div>
                                 <Image
                                     src="\images\home\arrow2.svg"
                                     alt="team_esa.svg"
@@ -94,14 +123,15 @@ export default function WhoAreWe() {
                                 />
                             </div>
                         </div>
-                        <div className="w-[80%] md:w-88">
-                            <p className="self-start text-sm tracking-[0.15em] text-black mb-4">
-                                We are one of the largest and most diverse social clubs on campus
-                                and welcome anyone to join us!
+                        <div className="w-[80%] max-w-[265px] md:max-w-none md:w-88 text-center">
+                            <p className="self-start text-sm text-black mb-4">
+                                ESA firmly believes that university life is not just about academic
+                                studies…but it&apos;s also about having fun, and meeting new
+                                friends!
                             </p>
-                            <p className="self-start text-sm tracking-[0.15em] text-black">
-                                We believe that university life is not just about academic
-                                studies... but also having fun, and meeting new friends.
+                            <p className="self-start text-sm text-black">
+                                Whether you&apos;re here to smash it at sports day, chill at pool
+                                night, or game it out at arcade night — we&apos;ve got you.
                             </p>
                         </div>
                     </div>
