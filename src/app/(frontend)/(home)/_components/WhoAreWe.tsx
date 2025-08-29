ec2ef8c29f3979631cd7b8a264f17e467a24504e
Joseph <j0sephw1532@gmail.com>
2025-08-29 16:14:11 +1200

    who are we mobile revision prototype

--- src/app/(frontend)/(home)/_components/WhoAreWe.tsx
+++ src/app/(frontend)/(home)/_components/WhoAreWe.tsx
@@ -1,99 +1,112 @@
-'use client'
+'use client';
 // import { useState, useEffect, useRef } from 'react'
-import Image from 'next/image'
-import Title from "@/components/ui/Title";
+import Image from 'next/image';
+import Title from '@/components/ui/Title';
 
 export default function WhoAreWe() {
-  // const [isLoaded, setIsLoaded] = useState(false)
-  // const imgRef = useRef<HTMLImageElement | null>(null)
+    // const [isLoaded, setIsLoaded] = useState(false)
+    // const imgRef = useRef<HTMLImageElement | null>(null)
 
-  // useEffect(() => {
-  //   if (imgRef.current && imgRef.current.complete) {
-  //     setIsLoaded(true)
-  //   }
-  // }, [])
+    // useEffect(() => {
+    //   if (imgRef.current && imgRef.current.complete) {
+    //     setIsLoaded(true)
+    //   }
+    // }, [])
 
-  return (
-    <div className="w-full flex justify-center md:w-auto">
-      <div className="relative w-full">
-        {/* bottom right frame*/}
-        <Image
-          src="/images/home/frame.png"
-          alt="top left frame"
-          width={60}
-          height={60}
-          className="absolute -bottom-10 -right-4 min-[490px]:-bottom-18 md:-bottom-10 w-[12%] md:w-[7.5%]"
-        />
+    return (
+        <div className="w-full flex justify-center md:w-auto">
+            <div className="relative w-full">
+                {/* bottom right frame*/}
+                <Image
+                    src="/images/home/frame.png"
+                    alt="top left frame"
+                    width={60}
+                    height={60}
+                    className="absolute -bottom-10 -right-4 min-[490px]:-bottom-18 md:-bottom-10 w-[12%] md:w-[7.5%]"
+                />
 
-        {/* top left frame */}
-        <Image
-          src="/images/home/frame.png"
-          alt="bottom right frame"
-          width={60}
-          height={60}
-          className="absolute -top-5 -left-9 min-[490px]:-top-14 md:-top-5 scale-x-[-1] scale-y-[-1] w-[12%] md:w-[7.5%] -rotate-14 md:-rotate-18"
-        />
+                {/* top left frame */}
+                <Image
+                    src="/images/home/frame.png"
+                    alt="bottom right frame"
+                    width={60}
+                    height={60}
+                    className="absolute -top-5 -left-9 min-[490px]:-top-14 md:-top-5 scale-x-[-1] scale-y-[-1] w-[12%] md:w-[7.5%] -rotate-14 md:-rotate-18"
+                />
 
-        {/* main box component */}
-        <div className="flex items-center justify-center gap-11 py-9 px-10 rounded-[3.5em] md:rounded-[5em] bg-[#ebe9e6]">
-          {/* background */}
-          <div className="absolute inset-0 bg-[url('/images/home/liquid_marbling_background.png')] bg-cover bg-center opacity-7 rounded-[3.5em] md:rounded-[5em]" />
+                {/* main box component */}
+                <div className="flex items-center justify-center gap-11 py-9 px-10 rounded-[3.5em] md:rounded-[5em] bg-[#ebe9e6]">
+                    {/* background */}
+                    <div className="absolute inset-0 bg-[url('/images/home/liquid_marbling_background.png')] bg-cover bg-center opacity-7 rounded-[3.5em] md:rounded-[5em]" />
 
-          {/* Who are we image */}
-          <div className="relative items-center hidden md:flex">
-            <Image
-              // ref={imgRef}
-              src="/images/home/team_photo.png"
-              alt="team photo"
-              width={320}
-              height={320}
-              // onLoad={() => setIsLoaded(true)}
-            />
-          </div>
+                    {/* Who are we image */}
+                    <div className="relative items-center hidden md:flex">
+                        <Image
+                            // ref={imgRef}
+                            src="/images/home/team_photo.png"
+                            alt="team photo"
+                            width={320}
+                            height={320}
+                            // onLoad={() => setIsLoaded(true)}
+                        />
+                    </div>
 
-          {/* Right side text */}
-          <div className="z-10 flex flex-col items-center gap-4">
-            <div className="relative">
-              {/* title */}
-              <Title className="bg-primary-red-400">
-                Who are we?
-              </Title>
-              {/* arrow */}
-              <Image
-                src="/images/home/arrow.png"
-                alt="arrow"
-                width={105}
-                height={105}
-                className="absolute top-[2rem] left-[-6.2rem] hidden md:block w-[33%]"
-              />
+                    {/* Right side text */}
+                    <div className="z-10 flex flex-col items-center gap-4">
+                        <div className="relative">
+                            {/* title */}
+                            <Title className="bg-primary-red-400">Who are we?</Title>
+                            {/* arrow */}
+                            <Image
+                                src="/images/home/arrow.png"
+                                alt="arrow"
+                                width={105}
+                                height={105}
+                                className="absolute top-[2rem] left-[-6.2rem] hidden md:block w-[33%]"
+                            />
+                        </div>
+
+                        {/* description */}
+                        <p className="w-[88%] md:w-56 mb-4 font-medium text-center text-black text-base/tight bg">
+                            ESA Social Club is your go-to community for fun, connection and a little
+                            friendly competition.
+                        </p>
+                        <div className="md:hidden relative w-90 flex justify-center px-4">
+                            <Image
+                                src="/images/home/who_are_we_mobile.png"
+                                alt="team photo"
+                                width={100}
+                                height={100}
+                                className="w-[90%] h-auto max-w-full object-contain"
+                            />
+                            <div className="flex flex-col">
+                                <Image
+                                    src="\images\home\team_esa.svg"
+                                    alt="team_esa.svg"
+                                    width={131}
+                                    height={150}
+                                />
+                                <Image
+                                    src="\images\home\arrow2.svg"
+                                    alt="team_esa.svg"
+                                    width={50}
+                                    height={15}
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[80%] md:w-88">
+                            <p className="self-start text-sm tracking-[0.15em] text-black mb-4">
+                                We are one of the largest and most diverse social clubs on campus
+                                and welcome anyone to join us!
+                            </p>
+                            <p className="self-start text-sm tracking-[0.15em] text-black">
+                                We believe that university life is not just about academic
+                                studies... but also having fun, and meeting new friends.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            {/* description */}
-            <p className="w-[80%] md:w-56 mb-4 font-medium text-center text-black text-base/tight bg">
-              ESA Social Club is your go-to community for fun, connection, and a little friendly
-              competition.
-            </p>
-            <div className="md:hidden relative w-90 flex justify-center px-4">
-              <Image
-                src="/images/home/who_are_we_mobile.png"
-                alt="team photo"
-                width={100}
-                height={100}
-                className="w-[90%] h-auto max-w-full object-contain"
-              />
-            </div>
-            <div className='w-[80%] md:w-88'>
-              <p className="self-start text-sm tracking-[0.15em] text-black md:mb-4">
-                We are one of the largest and most diverse social clubs on campus and welcome anyone to join us!
-              </p>
-              <p className="self-start text-sm tracking-[0.15em] text-black hidden md:block">
-                We believe that university life is not just about academic studies... but
-                also having fun, and meeting new friends.
-              </p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
