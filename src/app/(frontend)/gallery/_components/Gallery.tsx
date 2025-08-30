3bfbbeab1512ad631a9e4d04b79f829daf2c1407
Taks07 <yutakaoishi7@gmail.com>
2025-08-30 15:43:18 +1200

    feat: changed padding of the board

--- src/app/(frontend)/gallery/_components/Gallery.tsx
+++ src/app/(frontend)/gallery/_components/Gallery.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image"
+import Polaroid from "./Polaroid"
 
 export default function Gallery() {
     return (
-        <div className="relative w-[80vw] min-h-screen">
-        <Image
-            src="/images/gallery/board.png"
-            alt="Gallery Board Background"
-            fill
-            className="object-fill"
-            priority
-        />
-        <div className="relative z-10">
-            {/* Content goes here */}
+        <div>
+            <div className="relative w-[80vw] min-h-screen py-20 px-12">
+                <Image
+                    src="/images/gallery/board.png"
+                    alt="Gallery Board Background"
+                    fill
+                    className="object-fill"
+                    priority
+                />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
+                    <Polaroid />
+                    <Polaroid />
+                    <Polaroid variation="large" />
+                    <Polaroid />
+                    <Polaroid />
+                    <Polaroid variation="large" />
+                    <Polaroid variation="large" />
+                    <Polaroid />
+                    <Polaroid />
+                </div>
+            </div>
         </div>
-</div>
     )
 }
