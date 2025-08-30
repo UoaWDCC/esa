4f2eb1e80bbfdf655a04f70abec53fb95be27584
Taks07 <yutakaoishi7@gmail.com>
2025-08-30 14:59:21 +1200

    feat: polaroid now takes in image

--- src/app/(frontend)/gallery/_components/Polaroid.tsx
+++ src/app/(frontend)/gallery/_components/Polaroid.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Pin from "./Pin";
 
 const PIN_COLOURS = {
@@ -23,13 +24,36 @@ export interface PolaroidProps {
     variation: 'small' | 'large';
 }
 
-export default function Polaroid({ image, eventDate, eventName, pinColour = 'red', variation }: PolaroidProps) {
+export default function Polaroid({ image = "/images/aboutus/AboutUsImage.jpg", eventDate = "12/20/2015", eventName = "TestEvent", pinColour = 'red', variation = 'small' }: PolaroidProps) {
     return (     
-        <div className="bg-white rounded-md w-[700px] h-[350px] lg:w-[40s0px] drop-shadow-lg">
+        <div className={`relative bg-white rounded-md drop-shadow-lg ${
+            variation === 'large'  
+                ? 'w-[60%] aspect-[370/320]'  
+                : 'w-[60%] aspect-[260/290]'
+        }`}>
             <Pin 
-                className="absolute left-[45%] top-[-5%]" 
+                className="absolute left-[45%] top-[-6%]"
                 hexPinColour={PIN_COLOURS[pinColour]} 
             />
+            <div className="flex flex-col p-3 h-full">
+                <div className="relative w-full h-[80%]">
+                    <Image
+                        src={image}
+                        alt={eventName}
+                        fill
+                        sizes={variation === 'large' ? '370px' : '260px'}
+                        className="object-cover rounded-sm"
+                    />
+                </div>
+                <p className="mt-3 text-center text-[#2b2b2b] text-sm"> 
+                    <span>{new Date(eventDate).toLocaleDateString('en-AU', {
+                        year: 'numeric',
+                        month: 'numeric',
+                        day: 'numeric'
+                    })}</span>
+                    <span className="ml-1">{eventName}</span>
+                </p>
+            </div>
         </div>
     );
 }
