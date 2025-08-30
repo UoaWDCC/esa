44eb241a2cc6fe06c3b69a2890c4b4d4c6b24c0c
Joseph <j0sephw1532@gmail.com>
2025-08-30 16:37:58 +1200

    General Fixes

--- src/app/(frontend)/(home)/_components/WhoAreWe.tsx
+++ src/app/(frontend)/(home)/_components/WhoAreWe.tsx
@@ -1,37 +1,41 @@
 'use client';
-// import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image';
 import Title from '@/components/ui/Title';
 
 export default function WhoAreWe() {
-    // const [isLoaded, setIsLoaded] = useState(false)
-    // const imgRef = useRef<HTMLImageElement | null>(null)
-
-    // useEffect(() => {
-    //   if (imgRef.current && imgRef.current.complete) {
-    //     setIsLoaded(true)
-    //   }
-    // }, [])
-
     return (
         <div className="w-full flex justify-center md:w-auto">
             <div className="relative w-full">
                 {/* bottom right frame*/}
+                <Image
+                    src="/images/home/frame.png"
+                    alt="top left frame"
+                    width={60}
+                    height={60}
+                    className="absolute hidden md:block -bottom-10 -right-4 min-[490px]:-bottom-18 md:-bottom-10 w-[12%] md:w-[7.5%] "
+                />
+
+                {/* top left frame */}
+                <Image
+                    src="/images/home/frame.png"
+                    alt="bottom right frame"
+                    width={60}
+                    height={60}
+                    className="absolute hidden md:block -top-5 -left-9 min-[490px]:-top-14 md:-top-5 scale-x-[-1] scale-y-[-1] w-[12%] md:w-[7.5%] -rotate-14 md:-rotate-18"
+                />
 
                 {/* main box component */}
-                <div className="flex items-center justify-center gap-11 py-9 px-10 rounded-[3.5em] md:rounded-[5em] bg-[#ebe9e6]">
+                <div className="flex items-center justify-center gap-11 py-9 px-10 rounded-[3.5em] md:rounded-[5em] bg-primary-white">
                     {/* background */}
                     <div className="absolute inset-0 bg-[url('/images/home/liquid_marbling_background.png')] bg-cover bg-center opacity-7 rounded-[3.5em] md:rounded-[5em]" />
 
                     {/* Who are we image */}
                     <div className="relative items-center hidden md:flex">
                         <Image
-                            // ref={imgRef}
                             src="/images/home/team_photo.png"
                             alt="team photo"
                             width={320}
                             height={320}
-                            // onLoad={() => setIsLoaded(true)}
                         />
                     </div>
 
@@ -39,7 +43,7 @@ export default function WhoAreWe() {
                     <div className="z-10 flex flex-col items-center gap-4 mt-4">
                         <div className="relative">
                             {/* title */}
-                            <Title className="bg-primary-red-400 rounded-3xl p-5">
+                            <Title className="bg-primary-red-400 rounded-3xl px-5 py-4">
                                 Who are we?
                             </Title>
                             {/* arrow */}
@@ -50,12 +54,13 @@ export default function WhoAreWe() {
                                 height={105}
                                 className="absolute top-[2rem] left-[-6.2rem] hidden md:block w-[33%]"
                             />
+                            {/* mobile arrow */}
                             <Image
                                 src="/images/home/arrow3.svg"
                                 alt="arrow3.svg"
                                 width={39}
                                 height={100}
-                                className="absolute top-[2rem] left-[-3.4rem]"
+                                className="absolute top-[2rem] left-[-3.4rem] md:hidden"
                             />
                         </div>
 
@@ -64,7 +69,7 @@ export default function WhoAreWe() {
                             ESA Social Club is your go-to community for fun, connection and a little
                             friendly competition.
                         </div>
-
+                        {/* content image */}
                         <div className="md:hidden relative w-90 flex justify-center pl-3">
                             <Image
                                 src="/images/home/who_are_we_mobile.png"
@@ -73,7 +78,7 @@ export default function WhoAreWe() {
                                 height={100}
                                 className="w-[90%] h-auto max-w-full object-contain"
                             />
-
+                            {/* Top Right Particles */}
                             <div className="flex flex-col justify-center">
                                 <div className="relative">
                                     <Image
@@ -98,7 +103,7 @@ export default function WhoAreWe() {
                                     width={131}
                                     height={100}
                                 />
-                                {/* particles bottom left */}
+                                {/* Bottom Left Particles */}
                                 <div className="relative">
                                     <Image
                                         src="\images\home\frame_tr_small.svg"
@@ -115,6 +120,7 @@ export default function WhoAreWe() {
                                         className="absolute left-[-12px] bottom-[-2px] rotate-200"
                                     />
                                 </div>
+                                {/* The ESA Team Arrow */}
                                 <Image
                                     src="\images\home\arrow2.svg"
                                     alt="team_esa.svg"
