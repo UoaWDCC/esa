3edfab305c1b2e58a9a01ca95f9d0e85fb0d3423
Taks07 <yutakaoishi7@gmail.com>
2025-09-06 11:20:09 +1200

    chore: renamed function being exported in GalleryPage

--- src/app/(frontend)/gallery/page.tsx
+++ src/app/(frontend)/gallery/page.tsx
@@ -1,6 +1,6 @@
 import Gallery from "./_components/Gallery";
 
-export default function gallery() {
+export default function GalleryPage() {
     return (
         <div className="flex flex-col mt-[7vw] items-center">
             <h2 className="text-primary-red border-b-2 border-white mb-[2vw] px-[20vw]">Gallery</h2>
