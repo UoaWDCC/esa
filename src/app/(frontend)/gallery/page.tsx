de5f08d4da73f370e26c60f3b529a62cb36cc1d5
Taks07 <yutakaoishi7@gmail.com>
2025-09-06 11:14:15 +1200

    feat: Added separation line to gallery page

--- src/app/(frontend)/gallery/page.tsx
+++ src/app/(frontend)/gallery/page.tsx
@@ -3,7 +3,7 @@ import Gallery from "./_components/Gallery";
 export default function gallery() {
     return (
         <div className="flex flex-col mt-[10vw] items-center">
-            <h2 className="text-primary-red" >Gallery</h2>
+            <h2 className="text-primary-red border-b-2 border-white mb-[2vw] px-[20vw]">Gallery</h2>
             <Gallery /> 
         </div>
     );
