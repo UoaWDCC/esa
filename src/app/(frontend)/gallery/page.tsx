62b528fc9df2bc88034e33eac135a822dffb061b
Taks07 <yutakaoishi7@gmail.com>
2025-09-06 11:18:54 +1200

    chore: changed margins on the gallery page

--- src/app/(frontend)/gallery/page.tsx
+++ src/app/(frontend)/gallery/page.tsx
@@ -2,7 +2,7 @@ import Gallery from "./_components/Gallery";
 
 export default function gallery() {
     return (
-        <div className="flex flex-col mt-[10vw] items-center">
+        <div className="flex flex-col mt-[7vw] items-center">
             <h2 className="text-primary-red border-b-2 border-white mb-[2vw] px-[20vw]">Gallery</h2>
             <Gallery /> 
         </div>
