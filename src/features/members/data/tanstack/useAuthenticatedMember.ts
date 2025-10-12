22d34ca289e35c7d7d76f4873a5aa9aec7027747
Taks07 <yutakaoishi7@gmail.com>
2025-10-12 23:38:30 +1300

    feat: useAuthenticatedMember no longer disabled when not logged in

--- src/features/members/data/tanstack/useAuthenticatedMember.ts
+++ src/features/members/data/tanstack/useAuthenticatedMember.ts
@@ -5,17 +5,15 @@ import { useSession } from 'next-auth/react';
 import { getMemberByEmail } from '../payload/getMemberByEmail';
 
 export function useAuthenticatedMember() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const email = session?.user?.email;
 
-  // Always call useQuery
   return useQuery({
     queryKey: ['members', email],
     queryFn: async () => {
       if (!email) return null;
       return await getMemberByEmail(email);
     },
-    enabled: !!email && status === 'authenticated', 
     staleTime: 1000 * 60 * 10, // 10 minutes
   }).data;
 }
