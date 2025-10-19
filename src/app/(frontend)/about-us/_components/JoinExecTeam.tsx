9fec624e8b9d6b34d931a2ccb6276b6040f9b223
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-10-19 23:04:20 +1300

    more fixes from projectleads chat

--- src/app/(frontend)/about-us/_components/JoinExecTeam.tsx
+++ src/app/(frontend)/about-us/_components/JoinExecTeam.tsx
@@ -5,7 +5,7 @@ import PageTear from './PageTear';
 
 export default function JoinExecTeam() {
     return (
-        <div className="flex items-center justify-center md:justify-start md:min-h-[150vh] min-h-[100vh] relative">
+        <div className="flex items-center justify-center md:justify-start md:min-h-[150vh] min-h-[100vh] relative md:py-0 py-30">
             <Image
                 src="/images/aboutus/ExecTeamBg.jpg"
                 alt="exec team background"
@@ -15,7 +15,7 @@ export default function JoinExecTeam() {
                 style={{ objectPosition: '75% center' }}
             />
 
-            <div className="flex flex-col md:items-start items-center text-center md:text-left lg:ml-[13%] md:m-[10%] mx-[6%] lg:w-[45%] w-[full] gap-20 relative z-10">
+            <div className="flex flex-col md:items-start items-center text-center md:text-left lg:ml-[13%] md:m-[10%] mx-[6%] lg:w-[48%] w-[full] gap-5 relative z-10">
                 {/* First Half of text */}
                 <div className="flex flex-col gap-3 md:gap-5">
                     <h3 className="hidden md:block">Join our Exec Team</h3>
@@ -23,18 +23,15 @@ export default function JoinExecTeam() {
                         Join Our Exec Team
                     </h1>
                     <p className="text-2xl md:text-xl tracking-[0.15em] max-w-[24rem] mx-auto md:max-w-full md:mx-0">
-                        ESA is a social club is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ESA
-                        is a social club is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum
+                        Are you passionate about creating fun, inclusive events and making a real
+                        impact on campus? Join the ESA Executive Team and become part of a group of driven, creative students dedicated to bringing our community together. 
                     </p>
                 </div>
 
                 {/* Second Half of text */}
-                <div className="flex flex-col gap-2 md:gap-5">
+                <div className="flex flex-col gap-2 md:gap-5 mb-15">
                     <p className="text-2xl md:text-xl tracking-[0.15em] max-w-[24rem] mx-auto md:max-w-full md:mx-0">
-                        ESA is a social club is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum
+                        As an exec, you&apos;ll help plan and run exciting events, celebrate diverse cultures, and create opportunities for students to connect, learn, and grow. It&apos;s a chance to build leadership experience, make new friends, and shape the future of ESA on campus.
                     </p>
                 </div>
 
