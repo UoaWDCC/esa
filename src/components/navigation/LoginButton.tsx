0372570679cdcb816a599b37b3e22f361cef1bd4
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-10-20 12:36:27 +1300

    profile button

--- src/components/navigation/LoginButton.tsx
+++ src/components/navigation/LoginButton.tsx
@@ -1,19 +1,27 @@
 'use client';
 
-import { LogIn, LogOut } from 'lucide-react';
-import { signIn, signOut, useSession} from 'next-auth/react';
+import { LogIn, UserRound } from 'lucide-react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function LoginButton() {
     const { status } = useSession();
+    const router = useRouter();
 
     return (
         <div className="absolute top-4 right-4 z-[70]">
             <button 
                 className={"relative w-15 h-15 rounded-full flex items-center justify-center hover:cursor-pointer bg-primary-grey-light " + (status === 'loading' ? "disabled" : "")}
-                onClick={() => status === 'authenticated' ? signOut() : signIn('google')}
+                onClick={() => {
+                    if (status === 'authenticated') {
+                        router.push('/profile');
+                    } else {
+                        signIn('google');
+                    }
+                }}
             >
                 { status === 'authenticated' ?
-                    <LogOut className="w-6 h-6 transition-transform duration-200 hover:scale-110" /> :
+                    <UserRound className="w-8 h-8 transition-transform duration-200 hover:scale-110" strokeWidth={1.8}/> :
                     <LogIn className="w-6 h-6 transition-transform duration-200 hover:scale-110" />
                 }
             </button>
