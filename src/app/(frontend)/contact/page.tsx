c8189edae85ada83042105e5d1eeffffc7713c09
owenli <102705391+VoxelFS@users.noreply.github.com>
2026-01-11 12:21:34 +1300

    fix: fixed a bug where background star in contact form was overflowing

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from '@/app/(frontend)/contact/_components/ContactForm';
 
 export default function ContactPage() {
     return (
-        <section className="flex justify-center lg:px-[13%] md:px-[10%] px-[6%] h-full py-28 w-full">
+        <section className="relative flex justify-center lg:px-[13%] md:px-[10%] px-[6%] h-full py-28 w-full">
             <div className="flex flex-col md:flex-row items-stretch xl:items-center w-full">
                 <div className="mb-8 md:mb-0 md:mr-8 flex-1 flex md:w-[38%] lg:w-[40%] w-full">
                     <Image
@@ -17,9 +17,18 @@ export default function ContactPage() {
                         priority
                     />
                 </div>
-                <div className="w-full md:w-[62%] lg:w-[60%]">
+                <div className="w-full md:w-[62%] lg:w-[60%] overflow-hidden">
                     <ContactForm />
                 </div>
+            </div>
+            {/* Background star */}
+            <div className="absolute -bottom-50 -right-70 -z-10">
+                <Image
+                  src="/images/signup/background_star.png"
+                  alt="background star red"
+                  width={700}
+                  height={700}
+                />
             </div>
         </section>
     );
